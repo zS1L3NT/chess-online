d8b7bc6e4fa4baac3417408ddb8fe65fe9f0da8e
Zechariah <zechariahtan144@gmail.com>
2020-11-08 20:05:40 +0800

    Legal and Safe moves

--- src/board/Piece.java
+++ src/board/Piece.java
@@ -14,18 +14,30 @@ public abstract class Piece {
 
     public abstract List<Move> legal_moves(Board board);
 
-    public void set_position(int position) {
-        this.position = position;
-    }
-
-    public List<Integer> legal_positions(Board board) {
-        List<Integer> tiles = new ArrayList<Integer>();
+    public List<Move> safe_moves(Board board) {
         List<Move> legal_moves = legal_moves(board);
+        List<Move> moves = new ArrayList<Move>();
         for (int i = 0; i < legal_moves.size(); i++) {
             Move move = legal_moves.get(i);
+            Board simulation = move.simulate(board);
+            if (this.team.king_is_safe(simulation))
+                moves.add(move);
+        }
+        return moves;
+    }
+
+    public List<Integer> safe_positions(Board board) {
+        List<Integer> tiles = new ArrayList<Integer>();
+        List<Move> safe_moves = safe_moves(board);
+        for (int i = 0; i < safe_moves.size(); i++) {
+            Move move = safe_moves.get(i);
             tiles.add(move.destination);
         }
         return tiles;
+    }
+
+    public void set_position(int position) {
+        this.position = position;
     }
 
     @Override
--- src/board/Team.java
+++ src/board/Team.java
@@ -40,7 +40,9 @@ public enum Team {
     };
 
     public abstract int direction();
+
     public abstract boolean is_white();
+
     public boolean is_black() {
         return !is_white();
     }
@@ -65,18 +67,57 @@ public enum Team {
         List<Move> moves = new ArrayList<Move>();
         for (int i = 0; i < 64; i++) {
             Tile tile = board.getTile(i);
-            if (tile.is_empty()) continue;
-            if (tile.piece.team != this) continue;
+
+            // Skip if tile is empty
+            if (tile.is_empty())
+                continue;
+
+            // Skip if tile isnt on your team
+            if (tile.piece.team != this)
+                continue;
+
             List<Move> legal_moves = tile.piece.legal_moves(board);
-            if (legal_moves.size() == 0) continue;
-            for (int j = 0; j < legal_moves.size(); j++) {
+            if (legal_moves.size() == 0)
+                continue;
+            for (int j = 0; j < legal_moves.size(); j++)
                 moves.add(legal_moves.get(j));
-            }
         }
 
-        // TODO For each move, check if King is safe
+        return moves;
+    }
+
+    public List<Move> all_safe_moves(Board board) {
+        List<Move> moves = new ArrayList<Move>();
+        for (int i = 0; i < 64; i++) {
+            Tile tile = board.getTile(i);
+
+            // Skip if tile is empty
+            if (tile.is_empty())
+                continue;
+
+            // Skip if tile isnt on your team
+            if (tile.piece.team != this)
+                continue;
+
+            List<Move> safe_moves = tile.piece.safe_moves(board);
+            if (safe_moves.size() == 0)
+                continue;
+            for (int j = 0; j < safe_moves.size(); j++)
+                moves.add(safe_moves.get(j));
+        }
 
         return moves;
+    }
+
+    public boolean king_is_safe(Board board) {
+        int king_position = king_position(board);
+        List<Move> enemy_moves = enemy().all_legal_moves(board);
+        for (int i = 0; i < enemy_moves.size(); i++) {
+            Move move = enemy_moves.get(i);
+            if (move.destination == king_position)
+                return false;
+        }
+        return true;
     }
 
     public Team enemy() {
